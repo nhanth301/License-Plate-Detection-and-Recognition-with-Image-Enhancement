7a82bb5fb1e2d7d2afa9634439839762f2b68f36
Ng Thien Nhan <nhanthien.tnn@gmail.com>
2025-06-07 23:48:58 +0700

    update blurgen & train

--- models/blur_generator.py
+++ models/blur_generator.py
@@ -16,9 +16,9 @@ class AdaIN(nn.Module):
 class StyleEncoder(nn.Module):
     def __init__(self, in_channels=3, feature_dim=64):
         super(StyleEncoder, self).__init__()
-        self.enc1 = self.conv_block(in_channels, feature_dim)
-        self.enc2 = self.conv_block(feature_dim, feature_dim * 2)
-        self.enc3 = self.conv_block(feature_dim * 2, feature_dim * 4)
+        self.enc1 = self.conv_block(in_channels, feature_dim)          # 64 kênh
+        self.enc2 = self.conv_block(feature_dim, feature_dim * 2)     # 128 kênh
+        self.enc3 = self.conv_block(feature_dim * 2, feature_dim * 4) # 256 kênh
         self.pool = nn.MaxPool2d(2, 2)
 
     def conv_block(self, in_channels, out_channels):
@@ -35,10 +35,14 @@ class StyleEncoder(nn.Module):
         enc1 = self.enc1(blur_img)
         enc2 = self.enc2(self.pool(enc1))
         enc3 = self.enc3(self.pool(enc2))
-        
-        style_mean = torch.mean(enc3, dim=[2, 3], keepdim=True)
-        style_std = torch.std(enc3, dim=[2, 3], keepdim=True)
-        return style_mean, style_std
+     
+        style_mean1 = torch.mean(enc1, dim=[2, 3], keepdim=True)  # [B, 64, 1, 1]
+        style_std1 = torch.std(enc1, dim=[2, 3], keepdim=True)
+        style_mean2 = torch.mean(enc2, dim=[2, 3], keepdim=True)  # [B, 128, 1, 1]
+        style_std2 = torch.std(enc2, dim=[2, 3], keepdim=True)
+        style_mean3 = torch.mean(enc3, dim=[2, 3], keepdim=True)  # [B, 256, 1, 1]
+        style_std3 = torch.std(enc3, dim=[2, 3], keepdim=True)
+        return (style_mean1, style_std1), (style_mean2, style_std2), (style_mean3, style_std3)
 
 class ContentEncoder(nn.Module):
     def __init__(self, in_channels=3, feature_dim=64):
@@ -70,15 +74,15 @@ class BlurGenerator(nn.Module):
     def __init__(self, in_channels=3, out_channels=3, feature_dim=64):
         super(BlurGenerator, self).__init__()
         self.style_encoder = StyleEncoder(in_channels, feature_dim)
-        self.content_encoder = ContentEncoder(in_channels, feature_dim)
+        self.content_encoder = ContentEncoder(in_channels, feature_dim)  
 
         # Decoder
         self.upconv4 = nn.ConvTranspose2d(feature_dim * 8, feature_dim * 4, kernel_size=2, stride=2)
-        self.dec4 = self.conv_block(feature_dim * 8, feature_dim * 4)
+        self.dec4 = self.conv_block(feature_dim * 8, feature_dim * 4)  # 256
         self.upconv3 = nn.ConvTranspose2d(feature_dim * 4, feature_dim * 2, kernel_size=2, stride=2)
-        self.dec3 = self.conv_block(feature_dim * 4, feature_dim * 2)
+        self.dec3 = self.conv_block(feature_dim * 4, feature_dim * 2)  # 128 
         self.upconv2 = nn.ConvTranspose2d(feature_dim * 2, feature_dim, kernel_size=2, stride=2)
-        self.dec2 = self.conv_block(feature_dim * 2, feature_dim)
+        self.dec2 = self.conv_block(feature_dim * 2, feature_dim)      # 64 
         self.final_conv = nn.Conv2d(feature_dim, out_channels, kernel_size=1)
 
         self.adain = AdaIN()
@@ -95,21 +99,21 @@ class BlurGenerator(nn.Module):
         )
 
     def forward(self, clear_img, blur_img):
-        style_mean, style_std = self.style_encoder(blur_img)
+        (style_mean1, style_std1), (style_mean2, style_std2), (style_mean3, style_std3) = self.style_encoder(blur_img)
         enc1, enc2, enc3, enc4 = self.content_encoder(clear_img)
 
         dec4_up = self.upconv4(enc4)
-        dec4_mod = self.adain(dec4_up, style_mean, style_std)
+        dec4_mod = self.adain(dec4_up, style_mean3, style_std3)  # 256 
         dec4 = torch.cat([dec4_mod, enc3], dim=1)
         dec4 = self.dec4(dec4)
 
         dec3_up = self.upconv3(dec4)
-        dec3_mod = self.adain(dec3_up, style_mean, style_std)
+        dec3_mod = self.adain(dec3_up, style_mean2, style_std2)  # 128 
         dec3 = torch.cat([dec3_mod, enc2], dim=1)
         dec3 = self.dec3(dec3)
 
         dec2_up = self.upconv2(dec3)
-        dec2_mod = self.adain(dec2_up, style_mean, style_std)
+        dec2_mod = self.adain(dec2_up, style_mean1, style_std1)  # 64 
         dec2 = torch.cat([dec2_mod, enc1], dim=1)
         dec2 = self.dec2(dec2)
 
