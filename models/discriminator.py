cb8575d35eee23b1d2a8fb304a2244eb8f5d1acf
Ng Thien Nhan <nhanthien.tnn@gmail.com>
2025-06-10 15:37:35 +0700

    update blur_gen & train

--- models/discriminator.py
+++ models/discriminator.py
@@ -1,33 +1,48 @@
+# file: models/discriminator.py
 import torch
 import torch.nn as nn
+from torch.nn.utils import spectral_norm
 
 class Discriminator(nn.Module):
-    def __init__(self, in_channels=3):
+    def __init__(self, in_channels=3, features=[64, 128, 256, 512]):
         super(Discriminator, self).__init__()
-        self.features = nn.Sequential(
-            nn.Conv2d(in_channels, 64, kernel_size=4, stride=2, padding=1),  # (B, 64, H/2, W/2)
-            nn.LeakyReLU(0.2, inplace=True),
 
-            nn.Conv2d(64, 128, kernel_size=4, stride=2, padding=1),          # (B, 128, H/4, W/4)
-            nn.BatchNorm2d(128),
-            nn.LeakyReLU(0.2, inplace=True),
-
-            nn.Conv2d(128, 256, kernel_size=4, stride=2, padding=1),         # (B, 256, H/8, W/8)
-            nn.BatchNorm2d(256),
-            nn.LeakyReLU(0.2, inplace=True),
-
-            nn.Conv2d(256, 512, kernel_size=4, stride=2, padding=1),         # (B, 512, H/16, W/16)
-            nn.BatchNorm2d(512),
-            nn.LeakyReLU(0.2, inplace=True),
-        )
+        layers = []
+        in_ch = in_channels
         
-        self.classifier = nn.Sequential(
-            nn.AdaptiveAvgPool2d((1, 1)),  # (B, 512, 1, 1)
-            nn.Flatten(),                  # (B, 512)
-            nn.Linear(512, 1),             # (B, 1)
-            nn.Sigmoid()
+        for feature in features:
+            layers.append(
+                spectral_norm(
+                    nn.Conv2d(
+                        in_channels=in_ch,
+                        out_channels=feature,
+                        kernel_size=4,
+                        stride=2,
+                        padding=1,
+                        bias=False, 
+                    )
+                )
+            )
+
+            if in_ch != in_channels:
+                layers.append(nn.BatchNorm2d(feature))
+            
+            layers.append(nn.LeakyReLU(0.2, inplace=True))
+            in_ch = feature
+
+        layers.append(
+            spectral_norm(
+                nn.Conv2d(
+                    in_channels=in_ch,
+                    out_channels=1,
+                    kernel_size=4,
+                    stride=1,
+                    padding=1,
+                )
+            )
         )
 
-    def forward(self, img):
-        features = self.features(img)
-        return self.classifier(features)  # returns shape: [B, 1]
+        self.model = nn.Sequential(*layers)
+
+    def forward(self, x):
+        return self.model(x)
--- train.py
+++ train.py
@@ -11,13 +11,11 @@ from models.clear_generator import LPSR
 from models.blur_generator import BlurGeneratorMixture
 from models.discriminator import Discriminator
 from torchvision import models
-import shutil
 from tqdm import tqdm
 
 def kernel_regularization_loss(kernel):
-    with torch.no_grad():
-        tv_h = torch.sum(torch.abs(kernel[:, :, 1:, :] - kernel[:, :, :-1, :]))
-        tv_w = torch.sum(torch.abs(kernel[:, :, :, 1:] - kernel[:, :, :, :-1]))
+    tv_h = torch.sum(torch.abs(kernel[:, :, 1:, :] - kernel[:, :, :-1, :]))
+    tv_w = torch.sum(torch.abs(kernel[:, :, :, 1:] - kernel[:, :, :, :-1]))
     return tv_h + tv_w
 
 def train_models(blur_generator, discriminator, clear_generator,
@@ -30,20 +28,22 @@ def train_models(blur_generator, discriminator, clear_generator,
 
     l1_loss = nn.L1Loss()
     l2_loss = nn.MSELoss()
-    bce_loss = nn.BCELoss()
+    adversarial_loss = nn.BCEWithLogitsLoss()
 
     optimizer_blur_gen = optim.Adam(blur_generator.parameters(), lr=0.0002, betas=(0.5, 0.999))
     optimizer_clear_gen = optim.Adam(clear_generator.parameters(), lr=0.0002, betas=(0.5, 0.999))
-    optimizer_disc = optim.Adam(discriminator.parameters(), lr=0.0002, betas=(0.5, 0.999))
+    optimizer_disc = optim.Adam(discriminator.parameters(), lr=0.0001, betas=(0.5, 0.999))
 
-    scheduler_blur_gen = optim.lr_scheduler.StepLR(optimizer_blur_gen, step_size=10, gamma=0.5)
-    scheduler_clear_gen = optim.lr_scheduler.StepLR(optimizer_clear_gen, step_size=10, gamma=0.5)
-    scheduler_disc = optim.lr_scheduler.StepLR(optimizer_disc, step_size=10, gamma=0.5)
+    scheduler_blur_gen = optim.lr_scheduler.StepLR(optimizer_blur_gen, step_size=20, gamma=0.5)
+    scheduler_clear_gen = optim.lr_scheduler.StepLR(optimizer_clear_gen, step_size=20, gamma=0.5)
+    scheduler_disc = optim.lr_scheduler.StepLR(optimizer_disc, step_size=20, gamma=0.5)
 
     vgg = models.vgg16(pretrained=True).features.to(device).eval()
     
     for param in vgg.parameters():
         param.requires_grad = False
+
+    vgg_content_layers = vgg[:22]
 
     def perceptual_loss(img1, img2):
         vgg_mean = torch.tensor([0.485, 0.456, 0.406], device=img1.device).view(1, 3, 1, 1)
@@ -67,50 +67,52 @@ def train_models(blur_generator, discriminator, clear_generator,
         
         for batch_idx, (clear_img, blur_img) in enumerate(tqdm(dataloader, desc=f"Epoch {epoch+1}/{num_epochs}", leave=False)):
             clear_img, blur_img = clear_img.to(device), blur_img.to(device)
-            batch_size = clear_img.size(0)
-
-            fake_blur, blur_kernel = blur_generator(clear_img, blur_img)
-
+            
             optimizer_disc.zero_grad()
-            real_labels = torch.full((batch_size, 1), 0.9, device=device)
-            fake_labels = torch.zeros((batch_size, 1), device=device)
+            
+            fake_blur, _ = blur_generator(clear_img, blur_img)
+            
             real_output = discriminator(blur_img)
-            disc_real_loss = bce_loss(real_output, real_labels)
             fake_output = discriminator(fake_blur.detach())
-            disc_fake_loss = bce_loss(fake_output, fake_labels)
+
+            real_labels = torch.full_like(real_output, 0.9, device=device)
+            fake_labels = torch.zeros_like(fake_output, device=device)
+
+            disc_real_loss = adversarial_loss(real_output, real_labels)
+            disc_fake_loss = adversarial_loss(fake_output, fake_labels)
             disc_loss = (disc_real_loss + disc_fake_loss) / 2
+            
             disc_loss.backward()
-            torch.nn.utils.clip_grad_norm_(discriminator.parameters(), max_norm=1.0)
             optimizer_disc.step()
             
-            optimizer_blur_gen.zero_grad()
-            optimizer_clear_gen.zero_grad()
-            
-            gen_labels = torch.full((batch_size, 1), 1.0, device=device)
-            fake_output_for_gen = discriminator(fake_blur)
-            gen_adv_loss = bce_loss(fake_output_for_gen, gen_labels)
-
-            vgg_content_layers = vgg[:22]
-            clear_feats_content = vgg_content_layers(clear_img)
-            fake_feats_content = vgg_content_layers(fake_blur)
-            content_loss =  l1_loss(fake_feats_content, clear_feats_content)
-
-            hr_output = clear_generator(fake_blur)
-            sr_recon_loss = 5 * l1_loss(hr_output, clear_img) + l2_loss(hr_output, clear_img)
-            sr_perc_loss = perceptual_loss(hr_output, clear_img)
-            sr_loss = sr_recon_loss + 0.1 * sr_perc_loss
-            
-            k_reg_loss = kernel_regularization_loss(blur_kernel)
-            
-            total_gen_and_sr_loss = gen_adv_loss + 0.05 * content_loss + sr_loss + 0.0001 * k_reg_loss
-            
-            total_gen_and_sr_loss.backward()
-            torch.nn.utils.clip_grad_norm_(blur_generator.parameters(), max_norm=1.0)
-            torch.nn.utils.clip_grad_norm_(clear_generator.parameters(), max_norm=1.0)
-            optimizer_blur_gen.step()
-            optimizer_clear_gen.step()
-
             total_disc_loss += disc_loss.item()
+
+            for gen_update_step in range(2): 
+                optimizer_blur_gen.zero_grad()
+                optimizer_clear_gen.zero_grad()
+                
+                fake_blur, blur_kernel = blur_generator(clear_img, blur_img)
+                fake_output_for_gen = discriminator(fake_blur)
+                gen_labels = torch.ones_like(fake_output_for_gen, device=device)
+                gen_adv_loss = adversarial_loss(fake_output_for_gen, gen_labels)
+
+                clear_feats_content = vgg_content_layers(clear_img)
+                fake_feats_content = vgg_content_layers(fake_blur)
+                content_loss =  l1_loss(fake_feats_content, clear_feats_content)
+
+                hr_output = clear_generator(fake_blur)
+                sr_recon_loss = 5 * l1_loss(hr_output, clear_img) + l2_loss(hr_output, clear_img)
+                sr_perc_loss = perceptual_loss(hr_output, clear_img)
+                sr_loss = sr_recon_loss + 0.1 * sr_perc_loss
+                
+                k_reg_loss = kernel_regularization_loss(blur_kernel)
+                
+                total_gen_and_sr_loss = gen_adv_loss + 0.05 * content_loss + sr_loss + 0.0001 * k_reg_loss
+                
+                total_gen_and_sr_loss.backward()
+                optimizer_blur_gen.step()
+                optimizer_clear_gen.step()
+
             total_gen_loss += (gen_adv_loss + 0.05 * content_loss).item()
             total_sr_loss += sr_loss.item()
 
@@ -137,36 +139,26 @@ def train_models(blur_generator, discriminator, clear_generator,
             torch.save(clear_generator.state_dict(), os.path.join(save_path, f"sr_{epoch+1}.pth"))
 
         with torch.no_grad():
+            blur_generator.eval()
+            clear_generator.eval()
+            
+            fake_blur_vis, _ = blur_generator(clear_img, blur_img)
+            sr_img_vis = clear_generator(fake_blur_vis)
+
             clear_img_cpu = clear_img[0].detach().cpu()
-            fake_blur_cpu = fake_blur[0].detach().cpu()
+            fake_blur_cpu = fake_blur_vis[0].detach().cpu()
             real_blur_cpu = blur_img[0].detach().cpu()
-            sr_img_cpu = hr_output[0].detach().cpu()
+            sr_img_cpu = sr_img_vis[0].detach().cpu()
 
             def denormalize(img_tensor):
                 img_tensor = img_tensor.clamp(0, 1)
                 return img_tensor.permute(1, 2, 0).numpy()
 
             plt.figure(figsize=(20, 5))
-            plt.subplot(1, 4, 1)
-            plt.title("Clear Image (GT)")
-            plt.imshow(denormalize(clear_img_cpu))
-            plt.axis('off')
-
-            plt.subplot(1, 4, 2)
-            plt.title("Fake Blur")
-            plt.imshow(denormalize(fake_blur_cpu))
-            plt.axis('off')
-
-            plt.subplot(1, 4, 3)
-            plt.title("SR Image")
-            plt.imshow(denormalize(sr_img_cpu))
-            plt.axis('off')
-
-            plt.subplot(1, 4, 4)
-            plt.title("Real Blur")
-            plt.imshow(denormalize(real_blur_cpu))
-            plt.axis('off')
-
+            plt.subplot(1, 4, 1); plt.title("Clear Image (GT)"); plt.imshow(denormalize(clear_img_cpu)); plt.axis('off')
+            plt.subplot(1, 4, 2); plt.title("Fake Blur"); plt.imshow(denormalize(fake_blur_cpu)); plt.axis('off')
+            plt.subplot(1, 4, 3); plt.title("SR Image"); plt.imshow(denormalize(sr_img_cpu)); plt.axis('off')
+            plt.subplot(1, 4, 4); plt.title("Real Blur"); plt.imshow(denormalize(real_blur_cpu)); plt.axis('off')
             plt.tight_layout()
             plt.savefig(f"{vs_save_path}/output_epoch_{epoch+1}.png")
             plt.close()
