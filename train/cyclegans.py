c1ffb37b50e09b46045f1b6d598aae8322a9ad88
Ng Thien Nhan <nhanthien.tnn@gmail.com>
2025-06-25 10:09:33 +0700

    update train cyc

--- train/cyclegans.py
+++ train/cyclegans.py
@@ -14,7 +14,7 @@ from my_models.lpsr import LPSR
 
 def weights_init(m):
     classname = m.__class__.__name__
-    if classname.find("Conv") != -1:
+    if classname.find("Conv") != -1 and hasattr(m, "weight"):
         nn.init.normal_(m.weight.data, 0.0, 0.02)
     elif classname.find("BatchNorm") != -1:
         nn.init.normal_(m.weight.data, 1.0, 0.02)
